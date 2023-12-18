53914464125626d203123d082438aee221d06093
Daniel Amenyenu <ameneynudaniel45@gmail.com>
2023-12-18 12:32:30 +0000

    Navbar and Hero Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,72 @@
-import React from "react";
-
+import { TfiYoutube } from "react-icons/tfi";
+import { FaInstagram, FaFacebook, FaTiktok } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import Link from "next/link";
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="hero flex  justify-center px-2">
+      <div className="md:mt-[10rem] sm:mt-[8rem] mt-[6rem]">
+        <h1 className="text-white font-bold md:text-[130px] sm:text-[80px] text-[50px] text-center leading-[1]">
+          Soli Deo Gloria
+        </h1>
+        <p className="md:text-[50px] sm:text-[25px] text-[18px] italic font-bold text-white text-center">
+          Glory to God Alone
+        </p>
+
+        <div className="md:mt-[10rem] sm:mt-[8rem] mt-[6rem] px-[2rem]">
+          <p className="text-center text-white italic sm:text-[20px] text-[18px] mb-[1rem] ">
+            He made Him who knew no sin to be sin on our behalf, so that we
+            might become the righteousness of God in Him.
+          </p>
+          <p className="text-center text-white italic sm:text-[20px] text-[18px]">
+            2 Corinthians 5:21
+          </p>
+        </div>
+        <div>
+          <p className="text-white text-center sm:mt-[10rem] mt-[15rem] text-[20px] font-bold italic">
+            follow us
+          </p>
+          <div className="flex items-center justify-center sm:gap-[2rem] gap-[0.8rem] mt-[1rem]">
+            <Link
+              href={"https://www.youtube.com/@21stCenturyRevival"}
+              target="_blank"
+              className="text-white sm:text-[2.5rem] text-[1.8rem] cursor-pointer transition hover:scale-[1.09]"
+            >
+              <TfiYoutube />
+            </Link>
+            <Link
+              href={""}
+              target="_blank"
+              className="text-white sm:text-[2.5rem] text-[1.8rem] cursor-pointer transition hover:scale-[1.09]"
+            >
+              <FaInstagram />
+            </Link>
+            <Link
+              href={""}
+              target="_blank"
+              className="text-white sm:text-[2.5rem] text-[1.8rem] cursor-pointer transition hover:scale-[1.09]"
+            >
+              <FaFacebook />
+            </Link>
+            <Link
+              href={""}
+              target="_blank"
+              className="text-white sm:text-[2.5rem] text-[1.8rem] cursor-pointer transition hover:scale-[1.09]"
+            >
+              <FaXTwitter />
+            </Link>
+            <Link
+              href={""}
+              target="_blank"
+              className="text-white sm:text-[2.5rem] text-[1.8rem] cursor-pointer transition hover:scale-[1.09]"
+            >
+              <FaTiktok />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,77 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import { RiMenu3Line } from "react-icons/ri";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [menu, setMenu] = useState<boolean>(false);
+  return (
+    <nav>
+      <div className="flex items-center justify-between gap-[1rem] paddingNav py-2">
+        <div className="flex items-center cursor-pointer">
+          <img
+            src="/logo.jpg"
+            alt="logo"
+            className="xs:w-[50px] w-[30px] h-[30px] xs:h-[50px]"
+          />
+          <h2 className="font-bold xs:text-[20px] text-[17px] italic">
+            21st Century Revival
+          </h2>
+        </div>
+        <div className="hidden items-center gap-[1.5rem] sm:flex">
+          <a
+            href="#about"
+            className="italic font-medium text-[19px] transition  hover:text-[22px]"
+          >
+            About
+          </a>
+          <a
+            href="#contact"
+            className="italic font-medium text-[19px] transition hover:text-[22px]"
+          >
+            Contact
+          </a>
+          <a
+            href="#donate"
+            className="italic font-medium text-[19px] transition hover:text-[22px]"
+          >
+            Donate
+          </a>
+        </div>
+
+        <div
+          className="text-[2rem] cursor-pointer transition flex sm:hidden"
+          onClick={() => setMenu((prev) => !prev)}
+        >
+          <RiMenu3Line />
+        </div>
+      </div>
+      {menu && (
+        <div className="flex items-center flex-col justify-center w-[200px] gap-[1rem] absolute top-[70px] right-[10px] z-[100] flex sm:hidden">
+          <a
+            href="#about"
+            onClick={() => setMenu(false)}
+            className="italic font-medium text-[19px] transition  hover:text-[22px]"
+          >
+            About
+          </a>
+          <a
+            href="#contact"
+            onClick={() => setMenu(false)}
+            className="italic font-medium text-[19px] transition hover:text-[22px]"
+          >
+            Contact
+          </a>
+          <a
+            href="#donate"
+            onClick={() => setMenu(false)}
+            className="italic font-medium text-[19px] transition hover:text-[22px]"
+          >
+            Donate
+          </a>
+        </div>
+      )}
+    </nav>
+  );
 };
 
 export default Navbar;
